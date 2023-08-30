360866c4d2c9ef9b9d074187ba6e3312be3c5008
Richard Barbour <richyb07@gmail.com>
2023-08-30 11:25:21 -0700

    Pass test 1 and complete green phase.
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>
    Co-authored-by: Joey Palchak <joey.palchak@gmail.com>
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>

--- src/js/character.js
+++ src/js/character.js
@@ -1,15 +1,9 @@
 export class Character {
   constructor(strength, health, inventory) {
-    // this.strength = strength;
-    // this.health = health;
-    // this.inventory = inventory;
+    this.strength = strength;
+    this.health = health;
+    this.inventory = inventory;
   }
 }
 
 
-// class Triangle {
-//   constructor(side1, side2, side3) {
-//     this.side1 = side1;
-//     this.side2 = side2;
-//     this.side3 = side3;
-//   }

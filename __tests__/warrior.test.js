854d43ad359696e553d73bf279235a329e498374
Richard Barbour <richyb07@gmail.com>
2023-08-30 13:48:50 -0700

    Write test 4 and complete red phase.
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>
    Co-authored-by: Joey Palchak <joey.palchak@gmail.com>
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>

---
+++ __tests__/warrior.test.js
@@ -0,0 +1,12 @@
+import { Warrior } from "./../src/js/warrior.js"; 
+
+describe('Warrior', () => {
+
+
+    test('should create a warrior object with values', () => {
+      let warrior = new Warrior();
+      expect(warrior).toEqual({strength: 3, health: 100, inventory: "sword"});
+    });
+  
+
+  });
---
+++ src/js/warrior.js
@@ -0,0 +1,18 @@
+import { Character } from './character';
+
+export class Warrior extends Character {
+
+  constructor(strength, health, inventory) {
+    super(strength, health, inventory)
+    this.strength = strength;
+    this.health = health;
+    this.inventory = inventory;
+
+  }
+
+
+
+
+
+}
+

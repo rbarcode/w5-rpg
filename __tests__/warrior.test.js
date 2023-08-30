d484a168abb73708d43b910ee6de27341a4812f2
Richard Barbour <richyb07@gmail.com>
2023-08-30 13:54:45 -0700

    Refactor and pass test 4 and complete green phase.
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>
    Co-authored-by: Joey Palchak <joey.palchak@gmail.com>
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>

--- __tests__/warrior.test.js
+++ __tests__/warrior.test.js
@@ -5,7 +5,7 @@ describe('Warrior', () => {
 
     test('should create a warrior object with values', () => {
       let warrior = new Warrior();
-      expect(warrior).toEqual({strength: 3, health: 100, inventory: "sword"});
+      expect(warrior).toEqual({strength: 3, health: 100, inventory: 10});
     });
   
 
--- src/js/warrior.js
+++ src/js/warrior.js
@@ -4,9 +4,10 @@ export class Warrior extends Character {
 
   constructor(strength, health, inventory) {
     super(strength, health, inventory)
-    this.strength = strength;
-    this.health = health;
-    this.inventory = inventory;
+    this.strength = 3;
+    this.health = 100;
+    let sword = 10
+    this.inventory = sword;
 
   }
 
